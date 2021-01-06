4bdcf72a3c16cc42cea2116e2c431f8322f180f9
wujingyi588 <1311943687@qq.com>
2021-01-07 02:15:16 +0800

    第十一章例题

---
+++ src/exercise6/DistinctNumbers.java
@@ -0,0 +1,28 @@
+package exercise6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class DistinctNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list=new ArrayList<>();
+		
+		@SuppressWarnings("resource")
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter integers (input ends with 0): ");
+		int value;
+		
+		do {
+			value=input.nextInt();
+			
+			if(!list.contains(value) && value!=0)
+				list.add(value);
+		}while(value!=0);
+		
+		for(int i=0;i<list.size();i++) {
+			System.out.print(list.get(i)+" ");
+		}
+	}
+
+}
